2321d4c8017ce4caee291d0206c8606cd665b553
adibwp <adwirawp@gmail.com>
2023-08-09 15:19:37 +0700

    add validator

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -12,6 +12,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final loginController = Get.put(LoginController());
+  final GlobalKey<FormState> _formKey = GlobalKey();
   final TextEditingController _controllerUsername = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
 
@@ -40,79 +41,96 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Login',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 32,
-                      ),
-                    ),
-                    const Text('Please sign in to continue'),
-                    const SizedBox(height: 16.0,),
-                    TextFormField(
-                      controller: _controllerUsername,
-                      decoration: InputDecoration(
-                        labelText: 'User ID',
-                        hintText: 'User ID',
-                        hintStyle: const TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.w300,
-                        ),
-                        labelStyle: const TextStyle(
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Login',
+                        style: TextStyle(
                           fontWeight: FontWeight.w600,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(color: Colors.deepPurple)
+                          fontSize: 32,
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 16.0,),
-                    TextFormField(
-                      controller: _controllerPassword,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                        ),
-                        hintText: 'Password',
-                        hintStyle: const TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.w300,
-                        ),                      
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(color: Colors.deepPurple)
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16.0,),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.deepPurple,
-                        ),
-                        onPressed: (){
-                          loginController.login(_controllerUsername.text, _controllerPassword.text);
-                          Navigator.of(context).pushReplacementNamed('/homePage');
-                        }, 
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(
-                            color: Colors.white,
+                      const Text('Please sign in to continue'),
+                      const SizedBox(height: 16.0,),
+                      TextFormField(
+                        controller: _controllerUsername,
+                        decoration: InputDecoration(
+                          labelText: 'User ID',
+                          hintText: 'User ID',
+                          hintStyle: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                          ),
+                          labelStyle: const TextStyle(
                             fontWeight: FontWeight.w600,
-                            fontSize: 16,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(color: Colors.deepPurple)
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16.0,),
+                      TextFormField(
+                        controller: _controllerPassword,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                          hintText: 'Password',
+                          hintStyle: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                          ),                      
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(color: Colors.deepPurple)
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16.0,),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                          ),
+                          onPressed: (){
+                            if (_formKey.currentState?.validate() ?? false) {
+                              loginController.login(_controllerUsername.text, _controllerPassword.text);
+                              Navigator.of(context).pushReplacementNamed('/homePage');
+                            }
+                          }, 
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
